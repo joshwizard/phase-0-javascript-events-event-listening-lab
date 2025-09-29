9d3f0f7a68f5d0930af7145e1d6986bb876aac54
joshwizard <murikij@gmail.com>
2025-09-29 23:36:36 +0300

    Added code

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+
+const input = document.getElementById('button')
+
+function addingEventListener(e) {
+    input.addEventListener('click', e => alert('I was clicked'))
+    
 }
+addingEventListener()
+
